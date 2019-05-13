d2d2de5b68637821d085750ed0fe9e5ee8afdcfe
Daniel Radetsky <dradetsky@gmail.com>
2019-05-13 15:11:31 -0700

    add debug_parse; returns objs

--- parse_lisp/__init__.py
+++ parse_lisp/__init__.py
@@ -9,3 +9,18 @@ def parse(in_str):
     out = par.parse(toks)
 
     return out
+
+def debug_parse(in_str):
+    from .lexer import LispLexer
+    from .parser import LispParser
+
+    lex = LispLexer()
+    par = LispParser()
+
+    toks = lex.tokenize(in_str)
+    par._lexer = lex
+    out = par.parse(toks)
+
+    ret = [out, par, lex, toks]
+
+    return ret
